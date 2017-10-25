a5b026c8a5c3863397fa6f0039f39fae4c9adca3
novandypurnadrd <novandypurnadrd@gmail.com>
2017-10-25 10:42:05 +0700

    edit-config
    edit-config base url

--- PersonalMedicalRecord/application/config/config.php
+++ PersonalMedicalRecord/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PersonalMedicalRecord/';
+$config['base_url'] = 'http://10.3.5.100/PersonalMedicalRecord/';
 
 /*
 |--------------------------------------------------------------------------
